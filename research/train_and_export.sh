85c451dfbf5ac4cbaf504ffaff42e739a9913ce5
walter <walter.wang@imagr.co>
2023-06-07 16:12:53 +1200

    train and export

--- research/train_and_export.sh
+++ research/train_and_export.sh
@@ -5,13 +5,13 @@ export OUTPUT_DIR=$MODEL_DIR"/export"
 python3 object_detection/model_main.py \
     --logtostderr=true \
     --model_dir=$MODEL_DIR \
-    --pipeline_config_path=$MODEL_DIR/pipeline.config
+    --pipeline_config_path=$PIPELINE_CONFIG
 
 export LAST_CHECKPOINT=$MODEL_DIR/$(awk 'NR==1 && gsub("\"", "", $2) { print $2}' $MODEL_DIR/checkpoint)
 
 python3 object_detection/export_tflite_ssd_graph.py \
   --pipeline_config_path=$PIPELINE_CONFIG \
-  --trained_checkpoint_prefix=$MODEL_DIR/$LAST_CHECKPOINT \
+  --trained_checkpoint_prefix=$LAST_CHECKPOINT \
   --output_directory=$OUTPUT_DIR \
   --config_override " \
       model{ \
--- run_docker.sh
+++ run_docker.sh
@@ -2,5 +2,5 @@ export DATA="/home/walter/git/mobileDet/data/micro_controller/tfrecord"
 export TRAINED_MODEL="/home/walter/git/mobileDet/saved_models"
 sudo docker run -it --gpus device=0 \
 -v $DATA:/models/data/tfrecord \
--v $TRAINED_MODEL:/models/trained_model \
+-v $TRAINED_MODEL:/models/trained_models \
 od_tf1 /bin/bash 
